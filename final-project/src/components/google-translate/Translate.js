ceb8cc41eccfad2ea18947886c902ac27f5af5ad
Pete Andrew <112977421+Pete-Andrew@users.noreply.github.com>
2023-03-14 20:13:42 +0000

    added a translate page

--- final-project/src/components/google-translate/Translate.js
+++ final-project/src/components/google-translate/Translate.js
@@ -1,2 +1,24 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./translate.css"; 
+
+function Translate() {
+    return (
+  
+               
+  
+      <div id="about" className="container-fluid flex justify-content-center">
+       <h1>Here's the translation</h1>
+          
+          <p>This part is inside a .container class.</p> 
+          <p>The .container class provides a responsive fixed width container.</p>
+          <p>Resize the browser window to see that its width (max-width) will change at different breakpoints.</p>
+        </div>
+  
+        
+  
+     
+    );
+  }
+  
+  export default Translate;
