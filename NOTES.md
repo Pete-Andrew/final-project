a81c505dc1ed04eeefa7ff31aa5b3c7b9f596f1e
Pete <peter.a.andrew@gmail.com>
2023-03-15 19:15:03 +0000

    testing to see if my branch works

--- NOTES.md
+++ NOTES.md
@@ -3,3 +3,9 @@ npm i react
 npm i react-router-dom
 npm i jshint
 npm i speech-to-text
+
+npm install --legacy-peer-deps
+
+npm install --save react-google-translate @google-cloud/translate --legacy-peer-deps
+
+npm i react-speech-recognition
--- final-project/src/components/google-translate/Translate.js
+++ final-project/src/components/google-translate/Translate.js
@@ -1,24 +1,58 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./translate.css"; 
+import reportWebVitals from './reportWebVitals';
+import { setConfig } from 'react-google-translate';
 
-function Translate() {
+setConfig({
+  clientEmail: process.env.REACT_APP_GCP_CLIENT_EMAIL ?? '',
+  privateKey: process.env.REACT_APP_GCP_PRIVATE_KEY ?? '',
+  projectId: process.env.REACT_APP_GCP_PROJECT_ID ?? ''
+})
+
+const Example = () => {
+
+  const [text] = useState('test');
+  const [language] = useState('zh-CN');
+
+  const [translate, { data, loading }] = useLazyTranslate({
+    language
+  })
+
+  useEffect(() => {
+    if (text) {
+      translate(text, language);
+    }
+  }, [translate, text]);
+
+  render(); {
     return (
+      <div>{loading ? 'Loading...' : data}</div>
+    )
+  }
+}
+
+Example(); 
+
+// translate ();
+
+// function Translate() {
+//     return (
   
                
   
-      <div id="about" className="container-fluid flex justify-content-center">
-       <h1>Here's the translation</h1>
+//       <div id="about" className="container-fluid flex justify-content-center">
+//        <h1>Here's the translation</h1>
           
-          <p>This part is inside a .container class.</p> 
-          <p>The .container class provides a responsive fixed width container.</p>
-          <p>Resize the browser window to see that its width (max-width) will change at different breakpoints.</p>
-        </div>
+//           <p>This part is inside a .container class.</p> 
+//           <p>The .container class provides a responsive fixed width container.</p>
+//           <p>Resize the browser window to see that its width (max-width) will change at different breakpoints.</p>
+//         </div>
   
         
   
      
-    );
-  }
+//     );
+//   }
   
-  export default Translate;
+//   export default Translate;
